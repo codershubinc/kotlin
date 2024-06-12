572df70ca1c9c58cf3bbcbe99fba644d0d26dc45
Swapnil ingle <90494823+CodersHub-in@users.noreply.github.com>
2024-06-12 22:07:57 +0530

    basics in kotlin

--- kotlin_basics.kt
+++ kotlin_basics.kt
@@ -1,3 +1,15 @@
+
+
 fun  main(){
     println("hello world")
+    var age = 13
+    println(age)
+    age = 14
+    println(age)
+     age = 15
+    println(age)
+     age = 16
+    println(age)
+
+
 }
